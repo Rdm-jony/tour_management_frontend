1f671dd6ea060cd914eb83a59e034a0bbe4544dc
Rdm-jony <jonydascse@gmail.com>
2025-08-08 16:28:43 +0600

    project set up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+import CommonLayout from "./layout/commonLayout";
+
+function App() {
+  return (
+    <CommonLayout>
+      <Outlet />
+    </CommonLayout>
+  );
+}
+
+export default App;
